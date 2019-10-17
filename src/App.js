6bb075eb2aefd88938a1af27d704f60c3e479d80
Varun Jindal <vjindal0112@gmail.com>
2019-10-16 21:13:29 -0400

    stateful component, getting input from input, fetch broken

--- src/App.js
+++ src/App.js
@@ -1,42 +1,91 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './App.sass'
 
-function App() {
-  return (
-    <Fragment>
+class App extends Component {
 
-      <h1 className="title">Bulma</h1>
-      <p className="subtitle">
-        Modern CSS framework based on{' '}
-        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
-          Flexbox
-        </a>
-      </p>
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      error: null,
+      isLoaded: false,
+      items: []
+    };
 
-      <div className="field">
-        <div className="control">
-          <input className="input" type="text" placeholder="Input" />
-        </div>
-      </div>
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-      <div className="field">
-        <p className="control">
-          <span className="select">
-            <select>
-              <option>Select dropdown</option>
-            </select>
-          </span>
+  handleInput(event) {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("https://itunes.apple.com/search?" + this.state.value);
+    // fetch not working yet
+    fetch("https://itunes.apple.com/search?" + this.state.value)
+    .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.results
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+
+    alert('api: ' + this.state.items);
+  }
+
+  render() {
+    return (
+      <Fragment>
+
+        <h1 className="title">Bulma</h1>
+        <p className="subtitle">
+          Modern CSS framework based on{' '}
+          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
+            Flexbox
+          </a>
         </p>
-      </div>
 
-      <div className="buttons">
-        <a className="button is-primary">Primary</a>
-        <a className="button is-link">Link</a>
-      </div>
-    </Fragment>
-  );
+        <form onSubmit={this.handleSubmit}>
+          <div className="field">
+            <div className="control">
+              <input className="input" value={this.state.value} type="text" placeholder="Input" onChange={this.handleInput}/>
+            </div>
+          </div>
+        </form>
+
+        <div className="field">
+          <p className="control">
+            <span className="select">
+              <select>
+                <option>Select dropdown</option>
+              </select>
+            </span>
+          </p>
+        </div>
+
+        <div className="buttons">
+          <a className="button is-primary">Primary</a>
+          <a className="button is-link">Link</a>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
