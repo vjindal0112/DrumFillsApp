b409488c5788e0ab3b740316a4f81d0114b64841
Varun Jindal <vjindal0112@gmail.com>
2019-10-20 16:39:10 -0400

    tried some more for post requests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component, Fragment, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './App.sass'
+import React, { Component, Fragment, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./App.sass";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      valueTrack: '',
-      valueArtist: '',
+      valueTrack: "",
+      valueArtist: "",
       error: null,
       isLoaded: false,
       items: []
@@ -36,53 +35,50 @@ class App extends Component {
     fetch("https://accounts.spotify.com/api/token", {
       method: "POST",
       headers: {
-        Authorization: "Basic " + clientID + ":" + clientSecret,
-        type: "track",
-        q: this.state.valueTrack
+        Authorization: "Basic " + clientID + ":" + clientSecret
       },
       body: JSON.stringify({
-        // firstParam: "yourValue",
-        // secondParam: "yourOtherValue"
+        grant_type: "client_credentials"
       })
-    });
+    })
+      .then(function(response) {
+        console.log(response);
+        // return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+      });
   }
+  // fetch("https://itunes.apple.com/search?term=" + this.state.valueTrack)
+  // fetch("http://theaudiodb.com/api/v1/json/1/searchtrack.php?s=" + this.state.valueArtist + "&t=" + this.state.valueTrack)
+  // .then(res => res.json())
+  //   .then(
+  //     (res) => {
+  //       console.log(res.track);
+  //       this.setState({
+  //         isLoaded: true,
+  //         items: res.track
+  //       });
+  //     },
+  //     // Note: it's important to handle errors here
+  //     // instead of a catch() block so that we don't swallow
+  //     // exceptions from actual bugs in components.
+  //     (error) => {
+  //       this.setState({
+  //         isLoaded: true,
+  //         error: error
+  //       });
+  //     }
+  //   )
 
-
-
-    // fetch("https://itunes.apple.com/search?term=" + this.state.valueTrack)
-    // fetch("http://theaudiodb.com/api/v1/json/1/searchtrack.php?s=" + this.state.valueArtist + "&t=" + this.state.valueTrack)
-    // .then(res => res.json())
-    //   .then(
-    //     (res) => {
-    //       console.log(res.track);
-    //       this.setState({
-    //         isLoaded: true,
-    //         items: res.track
-    //       });
-    //     },
-    //     // Note: it's important to handle errors here
-    //     // instead of a catch() block so that we don't swallow
-    //     // exceptions from actual bugs in components.
-    //     (error) => {
-    //       this.setState({
-    //         isLoaded: true,
-    //         error: error
-    //       });
-    //     }
-    //   )
-
-    // }
-
-
-
+  // }
 
   render() {
     return (
       <Fragment>
-
         <h1 className="title">Bulma</h1>
         <p className="subtitle">
-          Modern CSS framework based on{' '}
+          Modern CSS framework based on{" "}
           <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
             Flexbox
           </a>
@@ -92,8 +88,20 @@ class App extends Component {
           <div className="field">
             <div className="control">
               <label>
-                <input className="input" value={this.state.valueTrack} type="text" placeholder="Track" onChange={this.handleInputTrack}/>
-                <input className="input" value={this.state.valueArtist} type="text" placeholder="Artist" onChange={this.handleInputArtist}/>
+                <input
+                  className="input"
+                  value={this.state.valueTrack}
+                  type="text"
+                  placeholder="Track"
+                  onChange={this.handleInputTrack}
+                />
+                <input
+                  className="input"
+                  value={this.state.valueArtist}
+                  type="text"
+                  placeholder="Artist"
+                  onChange={this.handleInputArtist}
+                />
               </label>
               <input type="submit" value="Submit" />
             </div>
