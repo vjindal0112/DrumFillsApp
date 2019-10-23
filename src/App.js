2a3d5558adbfe9b4b1c30bc7317ff9e63ae17ed2
Varun Jindal <vjindal0112@gmail.com>
2019-10-23 00:46:12 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Component, Fragment, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import "./App.sass";
